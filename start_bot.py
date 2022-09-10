fb5cf685714e790303d5eebf94072f3bd996b2e1
JustMyD <commonqued@gmail.com>
2022-09-10 20:20:58 +0300

    fix

--- start_bot.py
+++ start_bot.py
@@ -4,7 +4,7 @@ from aiogram.dispatcher.webhook import get_new_configured_app
 from aiogram.utils.executor import start_webhook
 from aiohttp import web
 
-from src.bot.init_bot import dp, on_shutdown, on_startup, WEBHOOK_URL
+from src.bot.init_bot import dp, on_shutdown, on_startup, WEBHOOK_PATH
 from src.bot.handlers import setup_dispatcher_handlers
 from src.bot.my_filters import setup_private_filter
 
@@ -14,7 +14,7 @@ setup_private_filter(dp)
 
 
 if __name__ == '__main__':
-    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_URL)
+    app = get_new_configured_app(dispatcher=dp, path=WEBHOOK_PATH)
     app.on_startup.append(on_startup)
     app.on_shutdown.append(on_shutdown)
     web.run_app(app, host=WEBAPP_HOST)
