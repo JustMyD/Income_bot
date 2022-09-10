7c680bf3b332632770d6604319e39b1bc2fc296b
JustMyD <commonqued@gmail.com>
2022-09-10 21:42:34 +0300

    fix path

--- start_bot.py
+++ start_bot.py
@@ -2,15 +2,20 @@ import os
 
 from aiogram.utils.executor import start_webhook
 
-from src.bot.init_bot import dp, on_shutdown, on_startup, WEBHOOK_PATH
+from src.bot.init_bot import dp, on_shutdown, on_startup
 from src.bot.handlers import setup_dispatcher_handlers
 from src.bot.my_filters import setup_private_filter
 
-WEBAPP_HOST = os.getenv('WEBAPP_HOST')
 setup_dispatcher_handlers(dp)
 setup_private_filter(dp)
+
+WEBHOOK_PATH = ''
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
 if __name__ == '__main__':
     start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
-                  skip_updates=True, host=WEBAPP_HOST)
+                  skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
