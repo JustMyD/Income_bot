29693be6b17487a1194ffd206dc6bd63efcbd6b4
JustMyD <commonqued@gmail.com>
2022-09-10 17:44:10 +0300

    Замена dp на state - data

--- src/bot/handlers/limits.py
+++ src/bot/handlers/limits.py
@@ -8,7 +8,6 @@ from aiogram.dispatcher.filters import Text
 from src.bot.keyboards.reply_keyboards import make_keyboard_reply
 from src.bot.keyboards.keyboards_mapping import REPLY_KEYBOARDS_MSGS
 
-from src.bot.init_bot import dp
 
 class FSMLimits(StatesGroup):
     limit_period = State()
@@ -29,14 +28,16 @@ async def limits_state_period(message: types.Message, state=FSMContext):
         keyboard = make_keyboard_reply(keyboard_level=message.text)
         await message.answer(text=REPLY_KEYBOARDS_MSGS[message.text], reply_markup=keyboard)
     else:
-        dp.storage.data['period'] = message.text
+        async with state.proxy() as data:
+            data['period'] = message.text
         await FSMLimits.next()
         rm_keyboard = types.ReplyKeyboardRemove()
         await message.answer(text=REPLY_KEYBOARDS_MSGS[message.text], reply_markup=rm_keyboard)
 
 
 async def limits_state_period_value(message: types.Message, state=FSMContext):
-    period = dp.storage.data['period']
+    async with state.proxy() as data:
+        period = data['period']
     tmp = json.load(open('../../../test.json', 'r'))
     tmp['limits'][period] = message.text
     json.dump(tmp, open('../../../test.json', 'w'), ensure_ascii=False, indent=3)
