de1da3bac24505998b1f966bed972498909d4d0a
JustMyD <commonqued@gmail.com>
2022-11-04 19:08:23 +0300

    fix callback_data

--- src/bot/handlers/reports.py
+++ src/bot/handlers/reports.py
@@ -19,29 +19,29 @@ async def show_report_type_message(message: types.Message):
     await message.answer(text='Выберите тип отчета', reply_markup=inline_message)
 
 
-async def show_report_period_message(query: types.CallbackQuery, inline_callback_data: dict):
-    report_type = inline_callback_data.get('type')
+async def show_report_period_message(query: types.CallbackQuery, callback_data: dict):
+    report_type = callback_data.get('type')
     report_message = 'прихода' if report_type == 'income' else 'расхода'
     inline_message = make_report_period_inline_message(report_type)
     await bot.edit_message_text(text=f'Выберите период для {report_message}', chat_id=query.message.chat.id, message_id=query.message.message_id, reply_markup=inline_message)
 
 
-async def show_today_report(query: types.CallbackQuery, inline_callback_data: dict):
-    report_type = inline_callback_data.get('type')
+async def show_today_report(query: types.CallbackQuery, callback_data: dict):
+    report_type = callback_data.get('type')
     user_id = query.message.from_user.id
     report = await get_today_report(user_id=str(user_id), report_type=report_type, msg_template=today_report_template)
     await query.message.answer(text=report, reply_markup=types.ReplyKeyboardRemove())
 
 
-async def show_weekly_report(query: types.CallbackQuery, inline_callback_data: dict):
-    report_type = inline_callback_data.get('type')
+async def show_weekly_report(query: types.CallbackQuery, callback_data: dict):
+    report_type = callback_data.get('type')
     user_id = query.message.from_user.id
     report = await get_weekly_report(user_id=str(user_id), report_type=report_type, msg_template=weekly_report_template)
     await query.message.answer(text=report, reply_markup=types.ReplyKeyboardRemove())
 
 
-async def show_monthly_report(query: types.CallbackQuery, inline_callback_data: dict):
-    report_type = inline_callback_data.get('type')
+async def show_monthly_report(query: types.CallbackQuery, callback_data: dict):
+    report_type = callback_data.get('type')
     user_id = query.message.from_user.id
     report = await get_monthly_report(user_id=str(user_id), report_type=report_type,
                                       msg_template=monthly_report_template)
