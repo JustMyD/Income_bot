1833b042f13971e57d15db78d1fd34a38b5bf884
WWW <commonqued@gmail.com>
2022-11-06 16:15:24 +0300

    fix

--- src/bot/handlers/income_main.py
+++ src/bot/handlers/income_main.py
@@ -21,8 +21,8 @@ async def income_state_start(message: types.Message):
 
 
 async def income_state_sum(message: types.Message, state: FSMContext):
-    income_sum = message.text
-    if income_sum.strip().lower() == 'отмена':
+    income_sum = message.text.strip().lower()
+    if income_sum == 'отмена':
         await state.finish()
         keyboard = make_keyboard_reply(keyboard_level='Главное меню')
         await message.answer(text='Отмена ввода', reply_markup=keyboard)
