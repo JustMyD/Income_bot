b4a6ecc5e0de0444b94e57bf8ac3e0c39619bd04
JustMyD <commonqued@gmail.com>
2022-11-08 21:29:22 +0300

    Добавил inline кнопку для возврата в главное меню

--- src/bot/handlers/main_menu.py
+++ src/bot/handlers/main_menu.py
@@ -1,5 +1,7 @@
+from bot.init_bot import bot
+from bot.keyboards.inline_keyboards import make_main_menu_keyboard, menu_callback_data
+
 from aiogram import types, Dispatcher
-from bot.keyboards.inline_keyboards import make_main_menu_keyboard
 
 
 async def show_main_menu(message: types.Message):
@@ -7,5 +9,12 @@ async def show_main_menu(message: types.Message):
     await message.answer(text='Выберите действие', reply_markup=inline_message)
 
 
+async def show_main_menu_inline(query: types.CallbackQuery):
+    inline_message = make_main_menu_keyboard()
+    await bot.edit_message_text(text='Выберите действие', chat_id=query.message.chat.id,
+                                message_id=query.message.message_id, reply_markup=inline_message)
+
+
 def register_handlers_main_menu(dp: Dispatcher):
     dp.register_message_handler(show_main_menu, commands='menu')
+    dp.register_callback_query_handler(show_main_menu_inline, menu_callback_data.filter(type='main_menu'))
