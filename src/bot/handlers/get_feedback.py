cf21e1f64ce9adce8c642ac4f4a0fc806d311f09
JustMyD <commonqued@gmail.com>
2022-11-12 23:06:51 +0300

    fix

--- src/bot/handlers/get_feedback.py
+++ src/bot/handlers/get_feedback.py
@@ -1,10 +1,12 @@
+import asyncio
+
 import yagmail
 from aiogram import Dispatcher, types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from bot.init_bot import bot
-from bot.keyboards.inline_keyboards import menu_callback_data
+from bot.keyboards.inline_keyboards import menu_callback_data, make_main_menu_keyboard
 from config.configuration import BOT_EMAIL_USERNAME
 
 
@@ -12,19 +14,30 @@ class FSMFeedback(StatesGroup):
     feedback_start = State()
 
 
-async def start_getting_feedback(query: types.CallbackQuery):
+async def start_getting_feedback(query: types.CallbackQuery, state: FSMContext):
     await FSMFeedback.feedback_start.set()
     await bot.edit_message_text(text='Введите сообщение', chat_id=query.message.chat.id,
                                 message_id=query.message.message_id)
+    async with state.proxy() as data:
+        data['main_message'] = query.message.message_id
 
 
 async def send_feedback_to_owner(message: types.Message, state=FSMContext):
     to = 'commonqued@gmail.com'
     subject = 'Отзыв пользователя от бота Приход/Расход'
     body = message.text
-    async with yagmail.SMTP(BOT_EMAIL_USERNAME, oauth2_file='/home/www/Bot_projects/Income_bot/src/config/oauth_creds.json') as mail:
+    with yagmail.SMTP(BOT_EMAIL_USERNAME, oauth2_file='/home/www/Bot_projects/Income_bot/src/config/oauth_creds.json') as mail:
         mail.send(to=to, subject=subject, contents=body)
-    await message.answer(text='Спасибо за ваш отзыв!')
+    bot_message = await message.answer(text='Спасибо за ваш отзыв!')
+    await asyncio.sleep(1)
+    inline_message = make_main_menu_keyboard()
+    chat_id = message.chat.id
+    async with state.proxy() as data:
+        await bot.edit_message_text(text='Выберите действие:            &#x200D;', chat_id=chat_id,
+                                    message_id=data['main_message'], reply_markup=inline_message)
+    await bot.delete_message(chat_id=chat_id, message_id=message.message_id)
+    await asyncio.sleep(2)
+    await bot.delete_message(chat_id=chat_id, message_id=bot_message.message_id)
     await state.finish()
 
 
