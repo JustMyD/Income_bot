8ea79347125c2eefe23c761a6cf498826a1a83a2
JustMyD <commonqued@gmail.com>
2022-11-12 16:48:20 +0300

    Рефакторинг, новая кнопка в меню

--- src/bot/handlers/__init__.py
+++ src/bot/handlers/__init__.py
@@ -1,7 +1,6 @@
 from aiogram import Dispatcher
 
-from bot.handlers.main_menu import register_handlers_main_menu
-from bot.handlers.preferences import register_preferences_handlers
+from bot.handlers.menu import register_handlers_main_menu
 from bot.handlers.reports import register_handlers_report
 from bot.handlers.limits import register_handlers_limits
 from bot.handlers.get_feedback import register_handlers_feedback
@@ -12,7 +11,6 @@ from bot.handlers.get_income import register_handlers_income
 
 def setup_dispatcher_handlers(dp: Dispatcher):
     register_handlers_main_menu(dp)
-    register_preferences_handlers(dp)
     register_handlers_report(dp)
     register_handlers_limits(dp)
     register_handlers_feedback(dp)
--- src/bot/handlers/menu.py
+++ src/bot/handlers/menu.py
@@ -1,11 +1,21 @@
-from bot.keyboards.inline_keyboards import menu_callback_data
 from bot.init_bot import bot
+from bot.keyboards.inline_keyboards import make_main_menu_keyboard, menu_callback_data
 
-from aiogram import Dispatcher, types
-from aiogram.types import CallbackQuery
+from aiogram import types, Dispatcher
 
 
-async def callback_show_preferences_menu(query: CallbackQuery):
+async def show_main_menu(message: types.Message):
+    inline_message = make_main_menu_keyboard()
+    await message.answer(text='Выберите действие:            &#x200D;', reply_markup=inline_message, parse_mode='HTML')
+
+
+async def show_main_menu_inline(query: types.CallbackQuery):
+    inline_message = make_main_menu_keyboard()
+    await bot.edit_message_text(text='Выберите действие:            &#x200D;', chat_id=query.message.chat.id, parse_mode='HTML',
+                                message_id=query.message.message_id, reply_markup=inline_message)
+
+
+async def callback_show_preferences_menu(query: types.CallbackQuery):
     inline_message = types.InlineKeyboardMarkup(inline_keyboard=[
         [types.InlineKeyboardButton(text='Отчеты', callback_data=menu_callback_data.new(type='reports',
                                                                                         action='show'))],
@@ -22,6 +32,9 @@ async def callback_show_preferences_menu(query: CallbackQuery):
                                 message_id=query.message.message_id, reply_markup=inline_message)
 
 
-def register_preferences_handlers(dp: Dispatcher):
+def register_handlers_main_menu(dp: Dispatcher):
+    dp.register_message_handler(show_main_menu, commands='menu')
+    dp.register_callback_query_handler(show_main_menu_inline, menu_callback_data.filter(type='main_menu',
+                                                                                        action='show'))
     dp.register_callback_query_handler(callback_show_preferences_menu, menu_callback_data.filter(type='preferences',
                                                                                                  action='show'))
